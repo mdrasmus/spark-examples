ec2bca5d2ecf617789b599dbcfef57a928b5dd15
Matt Rasmussen <rasmus@counsyl.com>
2016-08-03 10:42:44 -0700

    update

--- make.sh
+++ make.sh
@@ -21,6 +21,24 @@ wget -O src/spark-2.0.0-bin-hadoop2.7.tgz \
 cd src/
 tar zxvf spark-2.0.0-bin-hadoop2.7.tgz
 
+# Follow instructions.
+# http://spark.apache.org/docs/latest/spark-standalone.html#starting-a-cluster-manually
+
+# Start a master server.
+sbin/start-master.sh
+
+# Start a slave worker.
+SPARK_URL=spark://rasmus.local:7077
+sbin/start-slave.sh $SPARK_URL
+
+# Look at UI.
+open http://localhost:8080/
+
+# Start shell.
+./bin/spark-shell --master $SPARK_URL
+./bin/pyspark --master $SPARK_URL
+
+# Follow quick start tutorial here:
 # Tutorial: http://spark.apache.org/docs/latest/quick-start.html
 # Test the spark shell.
 bin/pyspark
